3857faeb3d8576a63e16134e0ef1d5efe5bb058d
Artist-MOBAI <mobai@duck.com>
2025-03-25 17:18:20 +0800

    feat: add GitHub link icon to Sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,3 +1,4 @@
+import Github from "@/components/icons/Github";
 import Link from "next/link";
 import React from "react";
 
@@ -37,12 +38,20 @@ const Sidebar = () => {
           Join
         </Link>
       </div>
-      <div className="pb-4">
+      <div className="pb-8">
         <div className="flex h-[1px] w-full">
           <div className="w-1/3 bg-[#D90000]" />
           <div className="w-1/3 bg-[#15DD4B]" />
           <div className="w-1/3 bg-[#3C00FF]" />
         </div>
+      </div>
+      <div className="px-4">
+        <Link
+          href="https://github.com/Team-intN18-SoybeanSeclab"
+          target="_blank"
+        >
+          <Github />
+        </Link>
       </div>
     </div>
   );
