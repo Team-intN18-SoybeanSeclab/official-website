5ccd0ca449f7572497985a4ece5d79bc20aa4f5f
Artist-MOBAI <mobai@duck.com>
2025-03-25 13:47:40 +0800

    refactor: separate Crosshair component into its own container for improved structure and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Contact from "@/sections/Contact";
 import Crosshair from "@/components/effects/Crosshair";
 import HomeSection from "@/sections/Home";
@@ -7,21 +5,11 @@ import Join from "@/sections/Join";
 import Links from "@/sections/Links";
 import Members from "@/sections/Members";
 import Sidebar from "@/components/layout/Sidebar";
-import { useRef } from "react";
 
 export default function Home() {
-  const containerRef = useRef<HTMLDivElement>(null);
   return (
     <>
-      <div
-        ref={containerRef}
-        className="h-screen w-full fixed top-0 left-0 z-20 pointer-events-none hidden sm:block"
-      >
-        <Crosshair
-          containerRef={containerRef as React.RefObject<HTMLElement>}
-          color="#e1fc30"
-        />
-      </div>
+      <Crosshair />
       <div className="flex flex-row h-screen w-full max-w-7xl mx-auto">
         <Sidebar />
         <div className="w-full overflow-y-auto overflow-x-hidden xl:border-r border-mobai-border scrollbar-hide">
--- components/effects/Crosshair.tsx
+++ components/effects/Crosshair.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { RefObject, useEffect, useRef } from "react";
 
 import { gsap } from "gsap";
@@ -222,4 +224,19 @@ const Crosshair: React.FC<CrosshairProps> = ({
   );
 };
 
-export default Crosshair;
+const CrosshairContainer = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  return (
+    <div
+      ref={containerRef}
+      className="h-screen w-full fixed top-0 left-0 z-20 pointer-events-none hidden sm:block"
+    >
+      <Crosshair
+        containerRef={containerRef as React.RefObject<HTMLElement>}
+        color="#e1fc30"
+      />
+    </div>
+  );
+};
+
+export default CrosshairContainer;
