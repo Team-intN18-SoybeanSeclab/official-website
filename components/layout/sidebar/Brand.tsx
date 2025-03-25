c5abcbe771b9320a367c60758dc2323475a8a94b
Artist-MOBAI <mobai@duck.com>
2025-03-25 18:09:57 +0800

    refactor: wrap Brand component in a Link for improved navigation and user experience in Sidebar

--- components/layout/sidebar/Brand.tsx
+++ components/layout/sidebar/Brand.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const Brand = () => {
   return (
     <div className="px-4 border-y border-mobai-border">
-      <div className="bg-mobai-yellow w-fit px-2 flex flex-col justify-center items-center">
+      <Link
+        href="/"
+        className="bg-mobai-yellow w-fit px-2 flex flex-col justify-center items-center"
+      >
         <p className="whitespace-nowrap text-black font-fjalla-one text-5xl">{`Soybean SEC`}</p>
-      </div>
+      </Link>
     </div>
   );
 };
